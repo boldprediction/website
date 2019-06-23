973fd29706a72a8bd5812bf10c9743eba942015b
atumu <atumu@andrew.cmu.edu>
2019-06-22 21:23:03 -0400

    minor bug fix

--- boldpredict/forms.py
+++ boldpredict/forms.py
@@ -121,5 +121,4 @@ class ResetForm(forms.Form):
         confirm_pwd = cleaned_data.get('confirm_pwd')
         if password and confirm_pwd and password != confirm_pwd:
             raise forms.ValidationError("Password and confirm password don't match.")
-        print("Here is the result: ",validate_password(password,password_validators = get_default_password_validators()))
         return cleaned_data
