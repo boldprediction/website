767c7d280b9e2dbb918a64390c7a38b62ae14099
atumu <atumu@andrew.cmu.edu>
2019-07-16 10:37:28 -0400

    Added connectionrefused exception handling

--- boldpredict/api/cache_api.py
+++ boldpredict/api/cache_api.py
@@ -15,26 +15,36 @@ def generate_hash_key(list1,list2):
 def add_contrast_into_cache(key,subject):
         #Connect to the client 
         client = base.Client(('localhost',11211))
-        result = client.set(key, json.dumps(subject),expire=86400) 
-        return result
+        try:
+                result = client.set(key, json.dumps(subject),expire=86400)  
+                return result      
+        except ConnectionRefusedError as cre:
+                print("Please start your memcache ",cre)
 
 # Check's if the key value pair exist in the memcache. If yes, then
 # it returns the value, else returns a None
 def check_contrast_in_cache(key):
         #Connect to the client 
         client = base.Client(('localhost',11211))
-        result = client.get(key)
-        if result is None:
-                return None
-        return json.loads(result)
+        try:
+                result = client.get(key)
+                if result is None:
+                        return None
+                return json.loads(result)
+        except ConnectionRefusedError as cre:
+                print("Please start your memcache ",cre)
+
 # Check's if the key value pair exist in the memcache. If yes, then
 # the key value pair is deleted and returns true, else returns 
 # a False if it doesn't exist 
 def delete_contrast_in_cache(key):
         #Connect to the client 
         client = base.Client(('localhost',11211))
-        result = client.delete(key)
-        return result 
+        try:
+                result = client.delete(key)
+                return result 
+        except ConnectionRefusedError as cre:
+                print("Please start your memcache ",cre)
 
 
 
