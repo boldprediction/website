983a0a82ba59398ff31e3205969af769543afbe1
Li Zhang <zhangli.cmu@gmail.com>
2019-09-30 13:30:10 -0400

    added migrate shell

--- setup.sh
+++ setup.sh
@@ -9,10 +9,4 @@ mkdir config
 python3 -m venv venv/
 source venv/bin/activate
 pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py makemigrations auth
-python manage.py migrate auth
-python manage.py makemigrations boldpredict
-python manage.py migrate boldpredict
 deactivate
