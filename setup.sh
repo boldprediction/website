b8442d13f4b63738dc4055eb0d71478145111700
Li Zhang <zhangli.cmu@gmail.com>
2019-09-30 13:22:59 -0400

    added setup shell

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python3 -m venv venv/
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py makemigrations auth
+python manage.py migrate auth
+python manage.py makemigrations boldpredict
+python manage.py migrate boldpredict
+deactivate
