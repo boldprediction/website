ed80d099af46f3172c653f8e903f13be7204876c
Weiwei Huang <weiweihu@andrew.cmu.edu>
2019-10-10 18:59:34 -0400

    remove test module

--- webapp/dev_settings.py
+++ webapp/dev_settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'boldpredict',
-    'rest_framework',
-    'tests'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
