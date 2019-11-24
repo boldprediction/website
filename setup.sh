208ba7fcfaaef9b89aafb9817a5665cb1956e575
Weiwei Huang <weiweihu@andrew.cmu.edu>
2019-11-23 23:03:22 -0800

    modify the setup

--- setup.sh
+++ setup.sh
@@ -12,19 +12,19 @@ cd ..
 
 pip install -r requirements.txt
 
-# installation
-if ! [ -x "$(command -v memcached)" ]; then
-  echo "Error: memcached is not installed."
-  sudo apt-get install memcached
-fi
+# # installation
+# if ! [ -x "$(command -v memcached)" ]; then
+#   echo "Error: memcached is not installed."
+#   sudo apt-get install memcached
+# fi
 
 
-# start
-ps -fe|grep memcached |grep -v grep
-if [ $? -ne 0 ]
-then
-memcached &
-echo "start memcached....."
-else
-echo "memcached is runing....."
-fi
+# # start
+# ps -fe|grep memcached |grep -v grep
+# if [ $? -ne 0 ]
+# then
+# memcached &
+# echo "start memcached....."
+# else
+# echo "memcached is runing....."
+# fi
